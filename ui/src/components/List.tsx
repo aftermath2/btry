c248bdd4dc58c449918ed06dcf79e1d1c62ca7a7
aftermath2 <cxbvnlrzfjwi@protonmail.com>
2024-03-18 15:09:12 -0300

    Create list component

---
+++ ui/src/components/List.tsx
@@ -0,0 +1,122 @@
+import { Component, Show, createEffect, createSignal, on, onCleanup, onMount } from "solid-js";
+import { SetStoreFunction, Store, createStore } from "solid-js/store";
+import { useI18n } from "@solid-primitives/i18n";
+
+import styles from "./List.module.css";
+import Loading from "./Loading";
+import Box from "./Box";
+import NoItems from "./NoItems";
+import Table from "./Table";
+import Pagination from "./Pagination";
+import { HandleError } from "../utils/actions";
+import { useAPIContext } from "../context/APIContext";
+
+interface Props {
+	itemsName: "bets" | "winners"
+	tableHeaders: string[]
+	getItems: (height: number) => Promise<any[]>
+	subscribe: (
+		items: Store<any>, setItems: SetStoreFunction<any>,
+		heights: Store<any>, setHeights: SetStoreFunction<number[]>,
+	) => void
+	showPagination?: boolean
+	hideTitleLink?: boolean
+	reset?: boolean
+}
+
+const List: Component<Props> = (props) => {
+	const api = useAPIContext()
+	const [t] = useI18n()
+	const heightOffset = props.itemsName === "bets" ? 1 : 2
+
+	const [index, setIndex] = createSignal(0)
+	// Not using resources because the values depend on each other
+	const [heights, setHeigths] = createStore<number[]>([])
+	const [items, setItems] = createStore<any[]>([])
+
+	const getHeights = async (): Promise<number[]> => {
+		const resp = await api.GetHeights()
+		setIndex(resp.heights.length - heightOffset)
+		return resp.heights
+	}
+
+	const onPaginationClick = async (next: boolean) => {
+		let i = next ? index() + 1 : index() - 1
+		setIndex(i)
+		const items = await props.getItems(heights[index()])
+		setItems(items)
+	}
+
+	// Reset the items list when the property changes
+	createEffect(
+		on(
+			() => props.reset,
+			() => setItems([])
+		)
+	)
+
+	onMount(async () => {
+		const heights = await getHeights()
+		setHeigths(heights)
+		const items = await props.getItems(heights[index()])
+		if (items !== undefined) {
+			setItems(items)
+		}
+
+		// Do not subscribe to events if we are in the items page
+		if (props.showPagination) {
+			return
+		}
+
+		props.subscribe(items, setItems, heights, setHeigths)
+	})
+
+	onCleanup(() => {
+		api.Close()
+	})
+
+	return (
+		<div
+			class={styles.container}
+			style={{ "flex-direction": props.showPagination ? "column" : "row" }}
+		>
+			<Box
+				title={t(props.itemsName)}
+				align="justify-between"
+				minWidth="18vw"
+				padding="25px"
+				margin="0"
+				titleFontSize="1.3rem"
+				titleHref={props.hideTitleLink ? undefined : `/${props.itemsName}`}
+			>
+				<Show when={items} fallback={<Loading />}>
+					<Show when={props.showPagination}>
+						<p class={styles.height}>
+							{`${t("block_height")}: ${heights[index()] || 0}`}
+						</p>
+					</Show>
+					<Show
+						when={items.length > 0}
+						fallback={<NoItems text={t(`no_${props.itemsName}`)} />}
+					>
+						<Table
+							headers={props.tableHeaders}
+							rows={items}
+						/>
+					</Show>
+				</Show>
+			</Box>
+
+			<Show when={props.showPagination}>
+				<Pagination
+					onClickPrev={HandleError(() => onPaginationClick(false))}
+					showPrev={index() > 0}
+					showNext={index() < heights.length - heightOffset}
+					onClickNext={HandleError(() => onPaginationClick(true))}
+				/>
+			</Show>
+		</div>
+	);
+};
+
+export default List;
--- ui/src/components/Winners.tsx
+++ ui/src/components/Winners.tsx
@@ -1,17 +1,11 @@
-import { Component, Show, createSignal, onCleanup, onMount } from "solid-js";
-import { createStore } from "solid-js/store";
+import { Component } from "solid-js";
+import { SetStoreFunction } from "solid-js/store";
 import { useI18n } from "@solid-primitives/i18n";
 import toast from 'solid-toast';
 
-import styles from "./Winners.module.css";
+import List from "./List";
 import { Winner } from "../types/winners"
-import Loading from "./Loading";
-import Box from "./Box";
-import NoItems from "./NoItems";
-import Table from "./Table";
-import Pagination from "./Pagination";
 import { BeautifyNumber } from "../utils/utils";
-import { HandleError } from "../utils/actions";
 import { useAuthContext } from "../context/AuthContext";
 import { useAPIContext } from "../context/APIContext";
 
@@ -25,45 +19,23 @@ const Winners: Component<Props> = (props) => {
 	const api = useAPIContext()
 	const [t] = useI18n()
 
-	const [index, setIndex] = createSignal(0)
-	// Not using resources because the values depend on each other
-	const [heights, setHeigths] = createStore<number[]>([])
-	const [winners, setWinners] = createStore<Winner[]>([])
-
-	const getHeights = async (): Promise<number[]> => {
-		const resp = await api.GetHeights()
-		setIndex(resp.heights.length - 2)
-		return resp.heights
-	}
-
-	const getWinners = async (): Promise<Winner[]> => {
-		const height = heights[index()]
+	const getWinners = async (height: number): Promise<Winner[]> => {
+		if (height === undefined) {
+			return []
+		}
 		const resp = await api.GetWinners(height)
 		return resp.winners
 	}
 
-	const onPaginationClick = async (next: boolean) => {
-		let i = next ? index() + 1 : index() - 1
-		setIndex(i)
-		const winners = await getWinners()
-		setWinners(winners)
-	}
-
-	onMount(async () => {
-		const heights = await getHeights()
-		setHeigths(heights)
-		const winners = await getWinners()
-		setWinners(winners)
-
-		// Do not subscribe to events if we are in the winners page
-		if (props.showPagination) {
-			return
-		}
-
+	const subscribeInfo = (
+		setWinners: SetStoreFunction<any>,
+		heights: any,
+		setHeights: SetStoreFunction<number[]>
+	): void => {
 		api.Subscribe("info", (payload) => {
 			if (payload.winners !== undefined) {
 				if (payload.next_height !== undefined) {
-					setHeigths(heights.length, payload.next_height)
+					setHeights(heights.length, payload.next_height)
 				}
 
 				setWinners(payload.winners)
@@ -81,48 +53,18 @@ const Winners: Component<Props> = (props) => {
 				)
 			}
 		})
-	})
-
-	onCleanup(() => {
-		api.Close()
-	})
+	}
 
 	return (
-		<div>
-			<Box
-				title={t("winners")}
-				align="justify-between"
-				minWidth="18vw"
-				padding="25px"
-				margin="0"
-				titleFontSize="1.3rem"
-				titleHref={props.hideTitleLink ? undefined : "/winners"}
-			>
-				<Show when={winners} fallback={<Loading />}>
-					<Show when={props.showPagination}>
-						<p class={styles.height}>
-							{heights[index()]}
-						</p>
-					</Show>
-					<Show when={winners.length > 0} fallback={<NoItems text={t("no_winners")} />}>
-						<Table
-							headers={[t("ticket"), t("nickname"), t("prize")]}
-							rows={winners}
-						/>
-					</Show>
-				</Show>
-			</Box>
-
-			<Show when={props.showPagination}>
-				<Pagination
-					onClickPrev={HandleError(() => onPaginationClick(false))}
-					showPrev={index() > 0}
-					showNext={index() < heights.length - 2}
-					onClickNext={HandleError(() => onPaginationClick(true))}
-				/>
-			</Show>
-		</div>
-	);
+		<List
+			itemsName="winners"
+			tableHeaders={[t("ticket"), t("nickname"), t("prize")]}
+			getItems={(height) => getWinners(height)}
+			subscribe={subscribeInfo}
+			showPagination={props.showPagination}
+			hideTitleLink={props.hideTitleLink}
+		/>
+	)
 };
 
 export default Winners;
